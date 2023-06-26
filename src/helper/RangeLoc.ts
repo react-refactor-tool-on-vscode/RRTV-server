63bec024810bb36f1278933ce827923e137f244c
Dawn Chan <dawnchan030920@outlook.com>
2023-06-27 02:16:46 +0800

    [Update] 1. Wrong algorithm.

--- src/helper/RangeLoc.ts
+++ src/helper/RangeLoc.ts
@@ -3,10 +3,12 @@ import * as t from "@babel/types";
 
 function IsRangeInLoc(range: Range, loc: t.SourceLocation): boolean {
     return (
-        loc.start.line <= range.start.line + 1 &&
-        loc.start.column <= range.start.character &&
-        loc.end.line >= range.end.line + 1 &&
-        loc.end.column >= range.end.character
+        (loc.start.line < range.start.line + 1 ||
+            (loc.start.line == range.start.line + 1 &&
+                loc.start.column <= range.start.character)) &&
+        (loc.end.line > range.end.line + 1 ||
+            (loc.end.line == range.end.line + 1 &&
+                loc.end.column >= range.end.character))
     );
 }
 
