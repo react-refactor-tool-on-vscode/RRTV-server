1decb1db317b70a1146cd4cb448c177965a9f914
Dawn Chan <dawnchan030920@outlook.com>
2023-06-27 02:19:23 +0800

    [Update] 1. Compute CodeAction.

--- src/handler/PropFlatten.ts
+++ src/handler/PropFlatten.ts
@@ -1,9 +1,10 @@
 import { BaseHandler, ContinuousOutputHandler } from "../interface/Handler";
 import { parse } from "@babel/parser";
-import traverse, { NodePath } from "@babel/traverse";
+import traverse, { NodePath, TraverseOptions } from "@babel/traverse";
 import * as t from "@babel/types";
 import {
     CodeAction,
+    CodeActionKind,
     CodeActionParams,
     Command,
     Range,
@@ -13,6 +14,7 @@ import {
     TextEdit,
 } from "vscode-languageserver";
 import { TextDocument } from "vscode-languageserver-textdocument";
+import { IsRangeInLoc } from "../helper/RangeLoc";
 
 class PropFlattenCodeActionHandler extends ContinuousOutputHandler<
     (CodeAction | Command)[],
@@ -43,21 +45,94 @@ function generateCodeAction(request: CodeActionParams): CodeAction | undefined {
     });
 
     const canBeDestructedResult = canBeDestructed(range, ast);
-    if (!canBeDestructedResult) return undefined;
+    if (!canBeDestructedResult.beDestructed) return undefined;
     else
         return CodeAction.create(
             "Prop Flatten",
             Command.create(
                 "Prop Flatten",
                 "rrtv.propFlatten",
-                request.textDocument.uri
-            )
+                request.textDocument.uri,
+                canBeDestructedResult.identifierName
+            ),
+            CodeActionKind.RefactorRewrite
         );
 }
 
 // TODO
-function canBeDestructed(range: Range, ast: t.Node): boolean {
-    return false;
+function canBeDestructed(
+    range: Range,
+    ast: t.Node
+): { beDestructed: boolean; identifierName: string } {
+    let isParamSelected: boolean = false;
+    let identifierName: string;
+    let hasTrailing: boolean = false;
+
+    const findIdentifier: TraverseOptions<t.Node> = {
+        Identifier(path) {
+            if (IsRangeInLoc(range, path.node.loc)) {
+                if (t.isObjectProperty(path.parent)) {
+                    if (
+                        t.isIdentifier(path.parent.value) &&
+                        path.node.name ==
+                            (path.parent.value as t.Identifier).name
+                    ) {
+                        isParamSelected = true;
+                        identifierName = path.node.name;
+                    }
+                } else if (t.isArrayPattern(path.parent)) {
+                    isParamSelected = true;
+                    identifierName = path.node.name;
+                }
+            }
+        },
+    };
+
+    const checkHasTrailing: TraverseOptions<t.Node> = {
+        Identifier(path) {
+            if (
+                t.isMemberExpression(path.parent) &&
+                t.isIdentifier(path.parent.object) &&
+                (path.parent.object as t.Identifier).name == path.node.name
+            ) {
+                hasTrailing = true;
+            }
+        },
+    };
+
+    traverse(ast, {
+        enter(path) {
+            if (t.isFunction(path.node) && IsRangeInLoc(range, path.node.loc)) {
+                let index: number;
+                path.node.params.forEach((param, id) => {
+                    if (IsRangeInLoc(range, param.loc)) {
+                        index = id;
+                    }
+                });
+
+                const paramChildPath = path.get(
+                    `params.${index}`
+                ) as NodePath<t.Node>;
+
+                if (t.isIdentifier(paramChildPath.node)) {
+                    isParamSelected = true;
+                    identifierName = paramChildPath.node.name;
+                } else {
+                    paramChildPath.traverse(findIdentifier);
+                }
+
+                if (!isParamSelected) return; // Ensure that the range is on a param.
+
+                // Check if it's references has trailing ones.
+                const bodyPath = path.get("body") as NodePath<t.Node>;
+                bodyPath.traverse(checkHasTrailing);
+            }
+        },
+    });
+    return {
+        beDestructed: hasTrailing,
+        identifierName: identifierName,
+    };
 }
 
 // TODO: Implement generateTextEdit
@@ -65,4 +140,4 @@ function generateTextEdit(): TextEdit[] {
     return [];
 }
 
-export { PropFlattenCodeActionHandler };
+export { PropFlattenCodeActionHandler, canBeDestructed };
--- src/test/propFlatten.test.ts
+++ src/test/propFlatten.test.ts
@@ -1,8 +1,11 @@
 import { parse } from "@babel/parser";
 import * as t from "@babel/types";
 import traverse, { NodePath } from "@babel/traverse";
+import generate from "@babel/generator";
+import { canBeDestructed } from "../handler/PropFlatten";
+import { Range } from "vscode-languageserver";
 
-const text = `function TodoItem({item: {source, text}}) {
+const text1 = `function TodoItem({item: {source, text}}) {
     return (
         <>
         <img src={source} />
@@ -11,7 +14,22 @@ const text = `function TodoItem({item: {source, text}}) {
     )
 }`;
 
-const ast = parse(text, {
+const ast1 = parse(text1, {
+    sourceType: "module",
+    errorRecovery: true,
+    plugins: ["jsx"],
+});
+
+const text2 = `function TodoItem(item) {
+    return (
+        <>
+        <img src={item.source} />
+        <span>{item.text}</span>
+        </>
+    )
+}`;
+
+const ast2 = parse(text2, {
     sourceType: "module",
     errorRecovery: true,
     plugins: ["jsx"],
@@ -20,7 +38,7 @@ const ast = parse(text, {
 it("find all jsx identifier", () => {
     const jsxIdentifiers: any[] = [];
 
-    traverse(ast, {
+    traverse(ast1, {
         JSXIdentifier(path) {
             jsxIdentifiers.push(path.node.name);
         },
@@ -32,7 +50,7 @@ it("find all jsx identifier", () => {
 it("find all identifier", () => {
     const identifiers: any[] = [];
 
-    traverse(ast, {
+    traverse(ast1, {
         Identifier(path) {
             identifiers.push(path.node.name);
         },
@@ -44,10 +62,10 @@ it("find all identifier", () => {
 it("get params type", () => {
     let result: string;
 
-    traverse(ast, {
+    traverse(ast1, {
         enter(path) {
             if (t.isFunction(path.node)) {
-                result = typeof(path.get('params'));
+                result = typeof path.get("params");
             }
         },
     });
@@ -58,17 +76,74 @@ it("get params type", () => {
 it("can get first param", () => {
     let result: boolean;
 
-    traverse(ast, {
+    traverse(ast1, {
         enter(path) {
             if (t.isFunction(path.node)) {
-                result = typeof(path.get('params.0')) != null;
+                result = typeof path.get("params.0") != null;
 
                 const params = path.node.params;
-                const first_param = path.get('params.0');
+                const first_param = path.get("params.0");
                 const a = "asd";
             }
-        }
-    })
+        },
+    });
 
     expect(result).toMatchSnapshot();
-})
+});
+
+it("functionTypeParam test", () => {
+    const ast = t.functionTypeParam(
+        t.identifier("id"),
+        t.stringTypeAnnotation()
+    );
+
+    const code = generate(ast).code;
+
+    expect(code).toMatchSnapshot();
+});
+
+it("path between parent and array(container)", () => {
+    let result: boolean = false;
+    const code = "function Test ([a,b]) {return a;}";
+    const ast = parse(code);
+    traverse(ast, {
+        Identifier(path) {
+            if (result == false) result = t.isArrayPattern(path.parent);
+        },
+    });
+
+    expect(result).toMatchSnapshot();
+});
+
+it("traverse function body", () => {
+    const identifiersInBody: any[] = [];
+
+    traverse(ast1, {
+        FunctionDeclaration(path) {
+            path.get("body").traverse({
+                Identifier(path) {
+                    identifiersInBody.push(path.node.name);
+                },
+            });
+        },
+    });
+
+    expect(identifiersInBody).toMatchSnapshot();
+});
+
+test("check param can be destructed", () => {
+    expect(canBeDestructed(Range.create(0, 19, 0, 19), ast2)).toEqual({
+        beDestructed: true,
+        identifierName: "item",
+    });
+
+    expect(canBeDestructed(Range.create(0, 0, 0, 0), ast2)).toEqual({
+        beDestructed: false,
+        identifierName: null,
+    });
+
+    expect(canBeDestructed(Range.create(0, 18, 0, 21), ast2)).toEqual({
+        beDestructed: true,
+        identifierName: "item",
+    });
+});
