7b8b33b36ad3b0c2b998c26812502f8d71358cd8
xiwen_youmu <1597083201@qq.com>
2023-06-27 09:30:14 +0800

    [update]attribute edit in one condition

--- src/server.ts
+++ src/server.ts
@@ -14,22 +14,25 @@ import { TextDocument } from "vscode-languageserver-textdocument";
 
 import createHandler from "./interface/CreateHandler";
 
-let connection = createConnection(ProposedFeatures.all);
+import { AttrEditHandler, addTabStop, generateSnippet } from "./handler/attributeEdit";
 
-let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+export let connection = createConnection(ProposedFeatures.all);
+
+export let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
 connection.onInitialize((params: InitializeParams) => {
     const result: InitializeResult = {
         capabilities: {
             textDocumentSync: TextDocumentSyncKind.Incremental,
-            completionProvider: {
-                resolveProvider: true,
-            },
-            hoverProvider: true,
+            hoverProvider: false,
             codeActionProvider: true,
             executeCommandProvider: {
                 // TODO: commands be clarified and filled.
-                commands: [],
+                commands: [
+                    "provide attribute", 
+                    "provide attribute exec",
+                    "run snippet"
+                ],
             },
         },
     };
@@ -39,6 +42,20 @@ connection.onInitialize((params: InitializeParams) => {
 
 // Usage: connection.onCodeAction(createHandler<(CodeAction | Command)[], CodeActionParams>([], []));
 
+connection.onCodeAction(createHandler<(CodeAction | Command)[], CodeActionParams> (
+    [
+        new AttrEditHandler()
+    ], 
+    []
+    )
+);
+
+connection.onExecuteCommand((params) => {
+    const command = params.command;
+    if(command === 'provide attribute exec') {
+        generateSnippet(params);
+    }
+})
 // Start listening.
 documents.listen(connection);
 connection.listen();
