211c5710211f0f3d9a618a820db71c59efc7f82b
Dawn Chan <dawnchan030920@outlook.com>
2023-06-28 00:25:28 +0800

    [Chore] 1. Move files.

---
+++ src/helper/canBeDestructed.ts
@@ -0,0 +1,81 @@
+import traverse, { NodePath, TraverseOptions } from "@babel/traverse";
+import * as t from "@babel/types";
+import { Range } from "vscode-languageserver";
+import { IsRangeInLoc } from "./RangeLoc";
+import { isHeadIdOfMemberExpr } from "./IsHeadIdOfMemberExpr";
+
+export function canBeDestructed(
+    range: Range,
+    ast: t.Node
+): { beDestructed: boolean; identifierName: string; isDirectParam: boolean } {
+    let isParamSelected: boolean = false;
+    let identifierName: string;
+    let hasTrailing: boolean = false;
+    let isDirectParam: boolean = false;
+
+    const findIdentifier: TraverseOptions<t.Node> = {
+        Identifier(path) {
+            if (IsRangeInLoc(range, path.node.loc)) {
+                if (t.isObjectProperty(path.parent)) {
+                    if (
+                        t.isIdentifier(path.parent.value) &&
+                        path.node.name ==
+                            (path.parent.value as t.Identifier).name
+                    ) {
+                        isParamSelected = true;
+                        identifierName = path.node.name;
+                    }
+                } else if (t.isArrayPattern(path.parent)) {
+                    isParamSelected = true;
+                    identifierName = path.node.name;
+                }
+            }
+        },
+    };
+
+    const checkHasTrailing: TraverseOptions<t.Node> = {
+        Identifier(path) {
+            if (isHeadIdOfMemberExpr(path)) {
+                hasTrailing = true;
+            }
+        },
+    };
+
+    traverse(ast, {
+        enter(path) {
+            if (t.isFunction(path.node) && IsRangeInLoc(range, path.node.loc)) {
+                let index: number;
+                path.node.params.forEach((param, id) => {
+                    if (IsRangeInLoc(range, param.loc)) {
+                        index = id;
+                    }
+                });
+
+                if (index == undefined) return;
+
+                const paramChildPath = path.get(
+                    `params.${index}`
+                ) as NodePath<t.Node>;
+
+                if (t.isIdentifier(paramChildPath.node)) {
+                    isParamSelected = true;
+                    identifierName = paramChildPath.node.name;
+                    isDirectParam = true;
+                } else {
+                    paramChildPath.traverse(findIdentifier);
+                }
+
+                if (!isParamSelected) return; // Ensure that the range is on a param.
+
+                // Check if it's references has trailing ones.
+                const bodyPath = path.get("body") as NodePath<t.Node>;
+                bodyPath.traverse(checkHasTrailing);
+            }
+        },
+    });
+    return {
+        beDestructed: hasTrailing,
+        identifierName: identifierName,
+        isDirectParam: isDirectParam,
+    };
+}
