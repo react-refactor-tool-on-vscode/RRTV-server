2f95a79b086c1adc90f49a1f8b135cd277a47cd2
Dawn Chan <dawnchan030920@outlook.com>
2023-06-29 14:50:58 +0800

    [Update] 1. Add apply edit.

--- src/handler/stateLifting.ts
+++ src/handler/stateLifting.ts
@@ -4,13 +4,17 @@ import { CodeActionParams } from "vscode-languageserver-protocol";
 import { connection, documents } from "../server";
 import { findAllParentComponentReferences } from "../helper/findAllParentComponentReferences";
 import { isCodeActionEnabled } from "../helper/isCodeActionEnabled";
-import { ExecuteCommandParams, TextEdit } from "vscode-languageserver";
+import {
+    ExecuteCommandParams,
+    TextDocumentEdit,
+    TextEdit,
+} from "vscode-languageserver";
 import { getTextEditsForStateLifting } from "../helper/getTextEditsForStateLifting";
 import parseToAst from "../helper/ParseToAst";
 import { checkParamIsSingleIdentifier } from "../helper/checkParamIsSingleIdentifier";
 
 const commandNameSent = "rrtv.stateLifting.0";
-const commandNameReceived = "rrtv.stateLifting.1"
+const commandNameReceived = "rrtv.stateLifting.1";
 
 class StateLiftingCodeActionHandler extends ContinuousOutputHandler<
     (CodeAction | Command)[],
@@ -31,7 +35,12 @@ class StateLiftingCodeActionHandler extends ContinuousOutputHandler<
                         commandNameSent,
                         request.textDocument.uri,
                         request.range,
-                        Array.from(findAllParentComponentReferences(code, request.range)),
+                        Array.from(
+                            findAllParentComponentReferences(
+                                code,
+                                request.range
+                            )
+                        ),
                         checkParamIsSingleIdentifier(code, request.range)
                     )
                 ),
@@ -59,6 +68,17 @@ class StateLiftingExecuteCommandHandler extends BaseHandler<
                 newId
             );
             // Apply the edits.
+            connection.workspace.applyEdit({
+                documentChanges: [
+                    TextDocumentEdit.create(
+                        {
+                            uri: uri,
+                            version: documents.get(uri).version,
+                        },
+                        edits
+                    ),
+                ],
+            });
         } else this.nextHandler.handle(null, request);
     }
 }
