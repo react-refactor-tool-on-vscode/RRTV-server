b19c4ab311758a41e690065aaea1d0ab9d4a53d3
Dawn Chan <dawnchan030920@outlook.com>
2023-07-01 04:34:06 +0800

    [hotfix] wrap calltocompose in try-catch.

--- src/handler/CallToCombineHandler.ts
+++ src/handler/CallToCombineHandler.ts
@@ -14,38 +14,46 @@ import {
     Position,
     WorkspaceChange,
 } from "vscode-languageserver/node";
-import { BaseHandler, ContinuousOutputHandler } from '../interface/Handler'
-import { connection, documents } from '../server'
+import { BaseHandler, ContinuousOutputHandler } from "../interface/Handler";
+import { connection, documents } from "../server";
 import { DocumentUri } from "vscode-languageserver-textdocument";
 import { hasJSXCode, addTabStop } from "../helper/attrInsertor";
-import { transformer } from '../helper/callToCombine'
-
+import { transformer } from "../helper/callToCombine";
 
 export class CallToCombineHandler extends ContinuousOutputHandler<
     (CodeAction | Command)[],
     CodeActionParams
 > {
-    protected concreteHandle(prevOutput: (CodeAction | Command)[], request: CodeActionParams): (CodeAction | Command)[] {
-        const range = request.range;
-        const uri = request.textDocument.uri;
-        const document = documents.get(uri);
-        const result = transformer(document.getText(), document.offsetAt(range.start))
-        console.log('result::: ', result);
-        if(!result.check) {return prevOutput;}
-        const change = new WorkspaceChange();
-        const a = change.getTextEditChange(uri);
-        a.replace(
-            Range.create(
-                Position.create(0, 0),
-                Position.create(1000, 300)
-            ),
-            result.newText
-        )
-        const codeAction = CodeAction.create(
-            "Component Call to Component Combine",
-            change.edit,
-            CodeActionKind.RefactorRewrite,
-        );
-        return [...(prevOutput ?? []), codeAction];
+    protected concreteHandle(
+        prevOutput: (CodeAction | Command)[],
+        request: CodeActionParams
+    ): (CodeAction | Command)[] {
+        try {
+            const range = request.range;
+            const uri = request.textDocument.uri;
+            const document = documents.get(uri);
+            const result = transformer(
+                document.getText(),
+                document.offsetAt(range.start)
+            );
+            console.log("result::: ", result);
+            if (!result.check) {
+                return prevOutput;
+            }
+            const change = new WorkspaceChange();
+            const a = change.getTextEditChange(uri);
+            a.replace(
+                Range.create(Position.create(0, 0), Position.create(1000, 300)),
+                result.newText
+            );
+            const codeAction = CodeAction.create(
+                "Component Call to Component Combine",
+                change.edit,
+                CodeActionKind.RefactorRewrite
+            );
+            return [...(prevOutput ?? []), codeAction];
+        } catch (error) {
+            console.log(JSON.stringify(error));
+        }
     }
 }
